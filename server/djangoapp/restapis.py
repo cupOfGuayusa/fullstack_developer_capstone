b9894dfd91768d1e7eca4e6935e252670a5f9ffb
cupofguayusa <cupofguayusa@gmail.com>
2025-12-12 01:05:30 -0500

    updated get_requests and analyze_review_sentiments

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,5 +1,5 @@
 # Uncomment the imports below before you add the function code
-# import requests
+import requests
 import os
 from dotenv import load_dotenv
 
@@ -14,9 +14,33 @@ sentiment_analyzer_url = os.getenv(
 # def get_request(endpoint, **kwargs):
 # Add code for get requests to back end
 
+def get_request(endpoint, *kwargs):
+    params = ""
+    if(kwargs):
+        for key,value in kwargs.items():
+            params=params.key+"="+value+"&"
+        
+    request_url =  backend_ url+endpoints+"?"+params
+
+    print("GET from {} ".format(request_url))
+    try:
+        response = requests.get(request_url)
+        return response.json()
+    except:
+        print("Network exeception occured")
+
 # def analyze_review_sentiments(text):
 # request_url = sentiment_analyzer_url+"analyze/"+text
 # Add code for retrieving sentiments
 
+def analyze_review_sentiments(text):
+    request_url = sentiment_analyzer_url+"analyze/"+text
+    try:
+        response = requests.get(request_url)
+        return response.json()
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
+        print("Network exception occurred")
+        
 # def post_review(data_dict):
 # Add code for posting review
