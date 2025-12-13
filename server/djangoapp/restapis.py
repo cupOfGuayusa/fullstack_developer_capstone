5c34c1373661808c9405b6bab423b734afc8e60b
cupofguayusa <cupofguayusa@gmail.com>
2025-12-13 16:21:04 -0500

    Fixed typo

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -20,7 +20,7 @@ def get_request(endpoint, *kwargs):
         for key,value in kwargs.items():
             params=params.key+"="+value+"&"
         
-    request_url =  backend_ url+endpoints+"?"+params
+    request_url =  backend_url+endpoints+"?"+params
 
     print("GET from {} ".format(request_url))
     try:
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -94,7 +94,7 @@ def get_dealer_details(request, dealer_id):
 # ...
 
 def add_review(request):
-    if (request.user.is.anonymous == False):
+    if (request.user.isAnonymous == False):
         data = json.loads(request.body)
         try:
             response = post_review(data)
